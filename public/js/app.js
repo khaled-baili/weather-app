6aff90726e4dfec4b384744a714e8a4ca183d20b
khaled-baili <bailikhaled@gmail.com>
2021-08-09 09:40:42 +0100

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 console.log('js script is loaded new')
     // const fetch = require('node-fetch')
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    })
-})
+    // fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    //     response.json().then((data) => {
+    //         console.log(data);
+    //     })
+    // })
 
 const weatherForm = document.querySelector('form')
 const search = document.getElementById('search-box')
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     messageOne.textContent = '....Loading'
     e.preventDefault()
-    fetch('http://localhost:3000/wheather?adress=' + location).then((response) => {
+    fetch('/wheather?adress=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = JSON.stringify(data.error)
